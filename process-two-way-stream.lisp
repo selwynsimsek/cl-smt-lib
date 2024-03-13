0644ea526ff7a6d07e7e2d8163f23443e48dee72
selwyn <selwyn.simsek@cantab.net>
2024-03-13 13:32:57 +0000

    ignore errors

--- process-two-way-stream.lisp
+++ process-two-way-stream.lisp
@@ -10,7 +10,8 @@
 ;;; A process wrapped in a two-way stream.
 (defclass process-two-way-stream (fundamental-two-way-stream)
   ((process :initarg :process :initform (error "process argument is required")
-            :reader process))
+            :reader process)
+   (thread :initarg :thread :accessor thread))
   (:documentation
    "A fundamental-two-way-stream wrapping a single process' input and output."))
 
@@ -22,12 +23,21 @@
                                  :error-output :stream
                                  :wait nil
                                  :search t)))
-    (let ((error-output *error-output*)
-          (error-input (process-info-error-output process)))
-      (bt:make-thread (lambda () (loop while (open-stream-p error-input) do (write-char (read-char error-input) error-output)))))
-    ;(setf *error-output* (make-broadcast-stream *error-output* ))
-    (make-instance 'process-two-way-stream
-                   #+ALLEGRO :element-type #+ALLEGRO '(unsigned-byte 8)
-                   :input (process-info-output process)
-                   :output (process-info-input process)
-                   :process process)))
+    (let* ((error-output *error-output*)
+           (error-input (process-info-error-output process))
+           (thread
+             (bt:make-thread
+              (lambda ()
+                (loop while (ignore-errors (open-stream-p error-input))
+                      do (ignore-errors (write-char (read-char error-input) error-output)))))))
+                                        ;(setf *error-output* (make-broadcast-stream *error-output* ))
+      (make-instance 'process-two-way-stream
+                     #+ALLEGRO :element-type #+ALLEGRO '(unsigned-byte 8)
+                     :input (process-info-output process)
+                     :output (make-broadcast-stream (process-info-input process)
+                                                    (open (pathname (format nil "/tmp/smt.~a.log"
+                                                                            (process-info-pid process)))
+                                                          :direction :output :if-exists :overwrite
+                                                          :if-does-not-exist :create))
+                     :thread thread
+                     :process process))))
